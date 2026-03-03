5e69f827e68a643310491402963bb1f1805f450b
Richard Feng <richardfeng43@gmail.com>
2026-03-02 21:57:45 -0800

    added everything to class and averaging hourly weather based on minutes

--- src/Backend/helpers.py
+++ src/Backend/helpers.py
@@ -1,5 +1,17 @@
+from typing import Any
+
+
 def relative_error(this, target):
     try:
         return abs(this - target) / target
     except ZeroDivisionError:
-        raise ZeroDivisionError("Can't have target value be 0")
+        return 0
+
+
+def get_sunset_time_average(minutes: int, category: str, sunsets: list[Any]):
+    weight_one: float = (60 - minutes)/60
+    weight_two: float = 1 - weight_one
+    ans: float = 0
+    ans += (sunsets[2]['values'][category]) * weight_one
+    ans += (sunsets[3]['values'][category]) * weight_two
+    return ans
