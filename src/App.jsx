f42f7a8db8e45c71fa838bb9b619bf292e46a41b
Richard Feng <richardfeng43@gmail.com>
2026-09-01 15:27:39 -0700

    developing simple frontend

--- src/App.jsx
+++ src/App.jsx
@@ -16,29 +16,22 @@ function App() {
       }
     )
   }, [])
+  const firstItem = data[0]?.['05/13 04:55 PM'] ?? []
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='mt-20'>
+      <h1 className='font-stretch-expanded text-4xl'>Sunsetology</h1>
+      <h3 class='mt-2 font-serif'>Know the sky before you look up.</h3>
+    </div>
+      <ul>
+        {firstItem.map(r => (<li>{r.category} : {r.desired} / {r.real}</li>))}
+      </ul>
+
+      <div className='flex mt-20 justify-center'>
+        <input className='inline-xl px-1 py-2 outline-none border-b border-stone-500 transition duration-300 ease-in-out focus:border-amber-200 text-3xl' placeholder="Irvine, CA"></input>
+        <button className='rounded-4xl ml-7 px-8 py-2 bg-amber-200 text-black hover:cursor-pointer shadow-sm shadow-amber-200/50 self-end transition duration-300 ease-in-out hover:bg-amber-300'>Check tonight</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
